d8bf0e3bdb32561ec16a3e4749e055d91f3d0372
Myktybek Omur <myktybekomurbekov@Myktybeks-MacBook-Pro.local>
2022-01-31 12:05:31 +0300

    add fatura odeme

---
+++ static-assets/js/paycell-odeme.js
@@ -0,0 +1,29 @@
+$('.removeSelect').on('click',function() {
+   
+    $(".empid").val('default');
+    $(".removeSelect").addClass(' hidden');
+    $(".nav_btn").removeClass('selected');
+    $(".visible").removeClass('show');
+    $(".visible").addClass('hidden');
+});
+
+  $('.empid').change(function (e) {
+    
+    console.log($(e.target).val());
+    if($(e.target).val()!="default")
+    {
+        $(".visible").removeClass('hidden');
+        $(".visible").addClass('show');
+        $(".nav_btn").removeClass('selected');
+        $("#"+$(e.target).val()).addClass('selected');
+        $(".removeSelect").removeClass(' hidden');
+    }
+    else{
+        $(".visible").removeClass('show');
+        $(".visible").addClass('hidden');
+        $(".nav_btn").removeClass('selected');
+        $(".removeSelect").addClass(' hidden');
+    }
+        
+    
+});
