37c68dfa3bd7fe8f0dae16a2c97b7692dbebe8a9
Myktybek Omur <myktybekomurbekov@Myktybeks-MacBook-Pro.local>
2022-02-14 09:27:20 +0300

    update home page,sss,paycell api,sdk

--- static-assets/js/main.js
+++ static-assets/js/main.js
@@ -1,19 +1,21 @@
-/*
-let money_btn = document.querySelector('#money_btn');
-let pay_btn = document.querySelector('#pay_btn');
-let wallet_btn = document.querySelector('#wallet_btn');
-let t_btn = document.querySelector('#turkcell_btn');
 
-money_btn.addEventListener('click', hidden('Money'));
-pay_btn.addEventListener('click', hidden('Pay'));
-wallet_btn.addEventListener('click', hidden('Wallet'));
-t_btn.addEventListener('click', hidden('Turkcell'));
+//search place open close
+$('.search-btn').on('click',function() {
+   
+    
+    $(".flex").addClass(' hidden');
+    $(".navbar-nav").addClass(' hidden');
+    $(".search-wrapper").removeClass('hidden');
 
-function hidden(idName) {
-    document.getElementById(idName).style.display = 'none';
-    console.log('1');
-}
-*/
+});
+$('.search-close').on('click',function() {
+   
+    
+    $(".search-wrapper").addClass(' hidden');
+    $(".navbar-nav").removeClass(' hidden');
+    $(".flex").removeClass(' hidden');
+
+});
 //search place open close
 $('.search-btn').on('click',function() {
    
@@ -65,36 +67,21 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+function selectItem(newId) {
+    $(".current").removeClass('current');
+    $("#"+newId).addClass(' current');
+    $(".visible").removeClass('visible');
+    $("."+newId).addClass(' visible');
 
-
-//sss
-var btnContainer = document.getElementById("btnDiv");
-var contentContainer = document.getElementById("content");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-var con = contentContainer.getElementsByClassName("visible");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    console.log(current);
-    current[0].className = current[0].className.replace(" active", "");
-    
-    this.className += " active";
-
-       
-    
-  });
 }
 
+//sss
+
 function changeVisible(newId) {
-    var current1 = document.getElementsByClassName("show");
-    current1[0].className = current1[0].className.replace(" show", "");
-    var elem = document.getElementById(newId);
-    console.log(elem);
-    elem.className += " show";
+    $(".show").removeClass('show');
+    $("#"+newId).addClass(' show');
+    $(".active").removeClass('active');
+    $("."+newId).addClass(' active');
 }
 
 
